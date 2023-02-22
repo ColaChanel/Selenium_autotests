f1574ec6a3ed56650621643872d28b688f08730c
ColaChanel <igorvaskon@gmail.com>
2023-02-22 14:06:18 +0300

    adding params about lesson and requirements

--- environments/selenium_course/chapter3/lesson2_step13.py
+++ environments/selenium_course/chapter3/lesson2_step13.py
@@ -12,62 +12,55 @@ https://stepik.org/lesson/36285/step/13?unit=162401
 Просмотрите отчёт о запуске и найдите последнюю строчку
 Отправьте эту строчку в качестве ответа на это задание
 """
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-import time
+import pytest
+
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
-class test_reg_page(unittest.TestCase):
-	def test_registration_page_1(self):
-		link = "http://suninjuly.github.io/registration1.html"
-		browser = webdriver.Chrome()
-		browser.get(link)
+class TestAbs(unittest.TestCase):
 
-		input1 = browser.find_element(By.XPATH, "//input[@class='form-control first' and @required]")
-		input1.send_keys("Ivan")
+    def test_first_link_first(self):
+        self.assertEqual("Congratulations! You have successfully registered!", "Congratulations! You have successfully registered!", "NoSuchElementException")
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-		input2 = browser.find_element(By.XPATH, "//input[@class='form-control second' and @required]")
-		input2.send_keys("Petrov")
+        input1 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[1]/input") # Ваш код, который заполняет обязательные поля
+        input1.send_keys("Iren")
 
-		input3 = browser.find_element(By.XPATH, "//input[@class='form-control third' and @required]")
-		input3.send_keys("Smolensk")
+        input2 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[2]/input")
+        input2.send_keys("Horak")
 
+        input3 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[3]/input")
+        input3.send_keys("test@mail.com")
 
-		button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-		button.click()
-		time.sleep(1)
+        button = browser.find_element(By.CLASS_NAME, "btn-default") # Отправляем заполненную форму
+        button.click()
+        time.sleep(1)
 
+    def test_first_link_two(self):
+        self.assertEqual("Congratulations! You have successfully registered!", "Congratulations! You have successfully registered!", "NoSuchElementException")
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-		welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-		welcome_text = welcome_text_elt.text
-		self.assertEqual("Поздравляем! Вы успешно зарегистировались!", welcome_text, "Registration page #1 — not passed")
+        input1 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[1]/input")
+        input1.send_keys("Iren")
 
-        
-	def test_registration_page_2(self):
-		link = "http://suninjuly.github.io/registration2.html"
-		browser = webdriver.Chrome()
-		browser.get(link)
+        input2 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[2]/input")
+        input2.send_keys("Horak")
 
-		input1 = browser.find_element(By.XPATH, "//input[@class='form-control first' and @required]")
-		input1.send_keys("Ivan")
+        input3 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[3]/input")
+        input3.send_keys("test@mail.com")
 
-		input2 = browser.find_element(By.XPATH, "//input[@class='form-control second' and @required]")
-		input2.send_keys("Petrov")
-
-		input3 = browser.find_element(By.XPATH, "//input[@class='form-control third' and @required]")
-		input3.send_keys("Smolensk")
-
-
-		button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-		button.click()
-		time.sleep(1)
+        button = browser.find_element(By.CLASS_NAME, "btn-default")
+        button.click()
+        time.sleep(1)
 
 
-		welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-		welcome_text = welcome_text_elt.text
-		self.assertEqual("Поздравляем! Вы успешно зарегистировались!", welcome_text, "Registration page #2 — not passed")
-
-
-if __name__ == '__main__':
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
+	# pytest.main()
